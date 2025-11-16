0a87eab64fd560f436d343b4b06d47f7c7a90a1c
Leopoldo Todisco <80098232+leotodisco@users.noreply.github.com>
2025-11-16 22:24:50 +0100

    update model path

--- app/service/classification_service.py
+++ app/service/classification_service.py
@@ -1,20 +1,25 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras.saving 
+from dotenv import find_dotenv, load_dotenv
+import os
 
-model = tensorflow.keras.saving.load_model("/Users/leopoldotodisco/TomatoCNN/models/small_CNN.keras")
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+model = tensorflow.keras.saving.load_model(os.getenv("model_path"))
 class_names = ["Damaged", "Old", "Ripe", "Unripe"]
 
 def get_prediction(img):
     
     logits = model(img, training=False)
-    probs = tf.nn.softmax(logits, axis=1)  # softmax esplicita
+    probs = tf.nn.softmax(logits, axis=1)
 
-    pred_idx = tf.argmax(probs, axis=1).numpy()[0]  # converto in int
+    pred_idx = tf.argmax(probs, axis=1).numpy()[0]
     pred_prob = probs[0, pred_idx].numpy()
 
-    print("Probabilità:", probs.numpy())
+    print("All probs:", probs.numpy())
     print("pred prob:", pred_prob)
-    print("pred prob:", class_names[pred_idx])
+    print("pred class:", class_names[pred_idx])
 
     return class_names[pred_idx]
